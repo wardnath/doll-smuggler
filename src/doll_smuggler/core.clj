be5e24ef37b0f3c54e0c976262f0d4da672d25ff
Nate Ward <wardnath.alt@gmail.com>
2015-07-24 20:46:05 -0500

    Added json parsing

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,5 +1,11 @@
 (ns doll-smuggler.core
+  (:require [cheshire.core :refer :all])
   (:gen-class))
+
+(def product_run 
+  (parse-stream 
+    (clojure.java.io/reader "./input_files/dolls_input_0.json")))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
