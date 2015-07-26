5031a303cb58f77dc82864b2217eb73b6a2d7631
Nate Ward <wardnath.alt@gmail.com>
2015-07-26 16:41:11 -0500

    added some more doll scenario tests

--- test/doll_smuggler/core_test.clj
+++ test/doll_smuggler/core_test.clj
@@ -22,6 +22,19 @@
     { :name "sally"   :weight 4   :value 50 } { :name "babe"     :weight 30 :value 10 }])
 
 
+;;one giant doll that shouldn't fit in the purse
+(def giant_doll 
+  [
+    { :name "godzilla"    :weight 90000   :value 150} ])
+
+
+;;very light dolls, all of them should fit in the purse
+(def light_dolls 
+  [
+    { :name "luke"    :weight 1   :value 100} { :name "anthony"  :weight 1 :value 200 }  
+    { :name "candice" :weight 1 :value 300} { :name "dorothy"  :weight 1 :value 400}])
+
+
 (deftest a-test
   (testing "Sanity Check"
     (is (= 1 1))))
@@ -31,15 +44,46 @@
           using code from https://github.com/mrozema/doll-smuggler")
 (deftest find-dolls-atomic
   (testing 
-    "Test the find-dolls algorithm against the data given by Atomic Object"
+    "Test the knapsack algorithm against the data given by Atomic Object"
     (let [[value indexes] (knapsack_calc (-> atomic_dolls count dec) 400 atomic_dolls)]
       ;; test existence of dolls
       (is (= (set indexes) #{0 1 2 3 4 6 10 15 16 17 18 20}))
       ;; test outputed total value
       (is (= value 1030))
+      ;; test the outputted total weight
+      (is (= (reduce + (map (comp :weight atomic_dolls) indexes)) 396))
       )
     )
   )
 
+(comment "Giant doll shouldn't be able to fit in the purse")
+(deftest find-dolls-giant
+  (testing 
+    "Test the knapsack algorithm against the giant doll data"
+    (let [[value indexes] (knapsack_calc (-> giant_doll count dec) 400 giant_doll)]
+      ;; test existence of dolls
+      (is (= (set indexes) #{}))
+      ;; test outputed total value
+      (is (= value 0))
+      ;; test the outputted total weight
+      (is (= (reduce + (map (comp :weight giant_doll) indexes)) 0))
+      )
+    )
+  )
+
+(comment "all dolls should fit in the purse")
+(deftest find-dolls-light
+  (testing 
+    "Test the knapsack algorithm against the light dolls data"
+    (let [[value indexes] (knapsack_calc (-> light_dolls count dec) 400 light_dolls)]
+      ;; test existence of dolls
+      (is (= (set indexes) #{0 1 2 3}))
+      ;; test outputed total value
+      (is (= value 1000))
+      ;; test the outputted total weight
+      (is (= (reduce + (map (comp :weight light_dolls) indexes)) 4))
+      )
+    )
+  )
 
 
