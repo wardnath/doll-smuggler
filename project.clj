0c6aa9ccde74f668edb3eb6c8389faae3704a44c
Nate Ward <wardnath.alt@gmail.com>
2015-07-26 13:17:03 -0500

    added file CLI param, changed product_run to be able to use user specified file

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                  [cheshire "5.5.0"] ]
+                  [cheshire "5.5.0"] 
+                  [org.clojure/tools.cli "0.3.1"]]
   :main ^:skip-aot doll-smuggler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,21 +1,47 @@
 (ns doll-smuggler.core
-  (:require [cheshire.core :refer :all])
+  (:require [cheshire.core :refer :all] 
+            [clojure.tools.cli :refer [parse-opts]]
+    )
+  (use [clojure.string :only [join]])
+  (use clojure.walk)
+  (use clojure.java.io)
   (:gen-class))
-(use '[clojure.string :only [join]])
-(use 'clojure.walk)
+
+(def cli-options
+  ;; An option with a required argument
+  [["-f" "--file FILE" "Input File"
+    :default "./input_files/dolls_input_0.json"
+    ;:parse-fn #(Integer/parseInt %)
+
+    :validate [#(.exists (as-file %1)) "Invalid File Path"]]]
+   ; ;; A non-idempotent option
+   ; ["-v" nil "Verbosity level"
+   ;  :id :verbosity
+   ;  :default 0
+   ;  :assoc-fn (fn [m k _] (update-in m [k] inc))]
+   ; ;; A boolean option defaulting to nil
+   ; ["-h" "--help"]]
+   )
 
 ;;parse json into clojure data structures
-(def product_run 
+;; example file: "./input_files/dolls_input_0.json"
+; (def product_run 
+;   (parse-stream 
+;     (clojure.java.io/reader (get options :file))))
+
+(defn product_run [file]
   (parse-stream 
-    (clojure.java.io/reader "./input_files/dolls_input_0.json")))
+    (clojure.java.io/reader file)))
 
 ;; define the vector of dolls based on the json data
-(def dolls
-    (keywordize-keys (get product_run "dolls")))
+(defn get_dolls [file]
+    (keywordize-keys (get (product_run file) "dolls")))
 
 ;; initial capacity of the handbag is equivalent to the max_weight json parameter
-(def capacity
-  (get product_run "max_weight"))
+(defn get_capacity [file]
+  (get (product_run file) "max_weight"))
+
+
 
 
 ;; code adopted from http://rosettacode.org/wiki/Knapsack_problem/0-1#Clojure
@@ -23,16 +49,16 @@
 
 (declare kc) ;forward decl for memoization function
  
-(defn knapsack_calc [i w]
+(defn knapsack_calc [i w dolls]
   (cond
     (< i 0) [0 []]
     (= w 0) [0 []]
     :else
     (let [{wi :weight vi :value} (get dolls i)]
       (if (> wi w)
-        (kc (dec i) w)
-        (let [[vn sn :as no]  (kc (dec i) w)
-              [vy sy :as yes] (kc (dec i) (- w wi))]
+        (kc (dec i) w dolls)
+        (let [[vn sn :as no]  (kc (dec i) w dolls)
+              [vy sy :as yes] (kc (dec i) (- w wi) dolls)]
           (if (> (+ vy vi) vn)
             [(+ vy vi) (conj sy i)]
             no))))))
@@ -41,11 +67,20 @@
 
 (defn -main
   [& args]
-  
+  (let [{:keys [options arguments]} (parse-opts args cli-options)]
 
-  (let [[value indexes] (knapsack_calc (-> dolls count dec) capacity)
-      names (map (comp :name dolls) indexes)]
-  (println "dolls to pack:" (join ", " names))
-  (println "total value:" value)
-  (println "total weight:" (reduce + (map (comp :weight dolls) indexes))))
+    (println (get options :file))
+
+
+
+    
+    (let [dolls (get_dolls (get options :file)) capacity (get_capacity (get options :file))]
+
+      (let [[value indexes] (knapsack_calc (-> dolls count dec) capacity dolls)
+          names (map (comp :name dolls) indexes)]
+      (println "dolls to pack:" (join ", " names))
+      (println "total value:" value)
+      (println "total weight:" (reduce + (map (comp :weight dolls) indexes))))
+      )
+    )
   )
