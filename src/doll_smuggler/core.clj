230cebdefc5e83411469af3b00a7a27b2c78790c
Nate Ward <wardnath.alt@gmail.com>
2015-07-25 19:33:43 -0500

    changed code to adopt from rosetta code solution

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,104 +1,51 @@
 (ns doll-smuggler.core
   (:require [cheshire.core :refer :all])
   (:gen-class))
-
-;; get the name of a doll at a given index
-(defn get_name [dolls index]
-  (get ( get dolls index) "name"))
-
-;; get the weight of a doll at a given index
-(defn get_weight [dolls index]
-  (get ( get dolls index) "weight"))
-
-;; get the value of a doll at a given index
-(defn get_value [dolls index]
-  (get ( get dolls index) "value"))
-
-;; generate vector of true values, this will be used to record the dolls that are taken
-(defn gen_true_vec [_iterator]
-  (def new_vec [])
-  (if (_iterator < 0)
-    new_vec
-    (
-      (conj new_vec true)
-      (def _iterator (_iterator - 1)))))
+(use '[clojure.string :only [join]])
+(use 'clojure.walk)
 
 ;;parse json into clojure data structures
 (def product_run 
   (parse-stream 
     (clojure.java.io/reader "./input_files/dolls_input_0.json")))
 
-;; set the index of this method to the index of the last element of the dolls container
-(def _index 
-  (- 
-    (count (get product_run "dolls"))
-    1))
-
 ;; define the vector of dolls based on the json data
-(def _dolls
-  (get product_run "dolls"))
+(def dolls
+    (keywordize-keys (get product_run "dolls")))
 
 ;; initial capacity of the handbag is equivalent to the max_weight json parameter
-(def _capacity
+(def capacity
   (get product_run "max_weight"))
 
-;; define a vector of booleans for each doll
-    ; (def _keep_vec
-    ;   (gen_true_vec 
-    ;     (count (get product_run "dolls"))
-    ;     )
-    ;   )
 
-(declare kc)
+;; code adopted from http://rosettacode.org/wiki/Knapsack_problem/0-1#Clojure
 
-(defn knapsack_calc [index_param dolls capacity keep_vec]
-  (if (< index_param 0) 
-    ;; if there are no more items return 0; stop recursing
-    0 
-    ;; else recursively search for better solution
-    ( last
-      ( list 
-        (if (< capacity (get_weight dolls index_param))
-          (def with_last_item -1) 
-          ;; else 
-          (def with_last_item
-            (+ 
-              (get_value dolls index_param) 
-              (kc 
-                (- index_param 1)
-                dolls
-                (- capacity (get_weight dolls index_param))
-                keep_vec )
-              )
-            )
-          )
 
-        (def without_last_item
-          (kc
-            (- index_param 1)
-            dolls
-            capacity
-            []
-            ; (assoc keep_vec index_param false)
-            ))
-        ;; return the better solution
-        (max with_last_item without_last_item)
-        )
-      )
-    )
-  )
-
+(declare kc) ;forward decl for memoization function
+ 
+(defn knapsack_calc [i w]
+  (cond
+    (< i 0) [0 []]
+    (= w 0) [0 []]
+    :else
+    (let [{wi :weight vi :value} (get dolls i)]
+      (if (> wi w)
+        (kc (dec i) w)
+        (let [[vn sn :as no]  (kc (dec i) w)
+              [vy sy :as yes] (kc (dec i) (- w wi))]
+          (if (> (+ vy vi) vn)
+            [(+ vy vi) (conj sy i)]
+            no))))))
+ 
 (def kc (memoize knapsack_calc))
 
-
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  ( 
-    
+  
 
-    (println (knapsack_calc _index _dolls _capacity []))
-
-    
-    )
-)
+  (let [[value indexes] (knapsack_calc (-> dolls count dec) capacity)
+      names (map (comp :name dolls) indexes)]
+  (println "dolls to pack:" (join ", " names))
+  (println "total value:" value)
+  (println "total weight:" (reduce + (map (comp :weight dolls) indexes))))
+  )
